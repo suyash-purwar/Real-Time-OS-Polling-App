2086023b4a094603d3235baf7141f0a48213aa77
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-03-13 20:32:24 +0530

    Setup app for heroku

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Map golbal promises
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://suyash:pusherpoll@ds213239.mlab.com:13239/pusherpoll' || 'mongodb://localhost:27017/PusherPoll')
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/PusherPoll')
     .then(() => {
         console.log('Connected to database');
     })
