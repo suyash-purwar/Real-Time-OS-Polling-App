283d2e9d39a02a534b9279084b268789c1f1000b
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-04-08 14:47:10 +0530

    Change the poll file url in fetch()

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ function getVoteAndSendToNode() {
             os: choice
         }
 
-        fetch('/poll' || 'localhost://27017/poll', {
+        fetch('../routes/poll', {
             method: 'post',
             body: JSON.stringify(data),
             headers: new Headers({
@@ -26,7 +26,7 @@ function getVoteAndSendToNode() {
 getVoteAndSendToNode();
 
 
-fetch('/poll' || 'localhost://poll')
+fetch('../routes/poll')
     .then(res => res.json())
     .then(data => {
         const votes = data.votes;
