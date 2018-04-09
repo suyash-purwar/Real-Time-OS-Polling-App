5eb62348d7d148867baf817e268d01097ed9b4f7
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-04-09 21:08:48 +0530

    Changed fetch url

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ function getVoteAndSendToNode() {
             os: choice
         }
 
-        fetch('/poll', {
+        fetch('https://fast-depths-22674.herokuapp.com/poll', {
             method: 'post',
             body: JSON.stringify(data),
             headers: new Headers({
@@ -26,7 +26,7 @@ function getVoteAndSendToNode() {
 getVoteAndSendToNode();
 
 
-fetch('/poll')
+fetch('https://fast-depths-22674.herokuapp.com/poll')
     .then(res => res.json())
     .then(data => {
         const votes = data.votes;
